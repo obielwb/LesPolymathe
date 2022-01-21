7cff20c572c63e4d2f254af5cef0faee6dc444af
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-01-21 12:30:50 -0300

    feat(Index): add information from each topic

---
+++ utils/topics.ts
@@ -0,0 +1,109 @@
+const line1 = [
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'Languages',
+    imageSrc: ''
+  },
+  {
+    title: 'Art',
+    imageSrc: ''
+  },
+  {
+    title: 'Culinary',
+    imageSrc: ''
+  },
+  {
+    title: 'Sports',
+    imageSrc: ''
+  },
+  {
+    title: 'Hobbies',
+    imageSrc: ''
+  },
+];
+
+const line2 = [
+  {
+    title: 'Cinema',
+    imageSrc: ''
+  },
+  {
+    title: 'News',
+    imageSrc: ''
+  },
+  {
+    title: 'Technology',
+    imageSrc: ''
+  },
+  {
+    title: 'Literature',
+    imageSrc: ''
+  },
+  {
+    title: 'Science',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+];
+
+const line3 =[
+  {
+    title: 'Health',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  }
+];
+
+const line4 = [
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  },
+  {
+    title: 'History',
+    imageSrc: ''
+  }
+];
+
+export { line1, line2, line3, line4 }
