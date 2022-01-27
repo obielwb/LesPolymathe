f1cf4f0ca82bd29e5087d141021286ed8a6a12c4
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-01-27 09:46:41 -0300

    feat: add historic page

---
+++ pages/historic.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+const Historic: NextPage = () => {
+  return(
+    <div>
+      <main className='app-historic'>
+        <p>Nothing here... <b>yet.</b></p>
+      </main>
+    </div>
+  )
+}
+
+export default Historic;
