afff33b4de4eed7b5f67ca6becf413a9cbdd057b
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-01-29 18:21:41 -0300

    feat: add value and onChange parameters on input

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,8 +32,19 @@ const Home: NextPage = () => {
               <p>Wanna receive the knowledge to became a polymath?</p>
               <p id="subscription-title">Subscribe in our <br /> newsletter</p>
               <form action="">
-                <input type="text" name="userName" id="user-name" maxLength={20} placeholder="Name" />
-                <input type="email" value="" placeholder="youremail@example.com" />
+                <input
+                  type="text"
+                  name="userName"
+                  id="user-name"
+                  maxLength={20}
+                  placeholder="Name"
+                  value={userName}
+                  onChange={event => setUserName(event.target.value)} />
+                <input
+                  type="email"
+                  value={userEmail}
+                  placeholder="youremail@example.com"
+                  onChange={event => setUserEmail(event.target.value)} />
                 <button type="submit">Subscribe</button>
               </form>
             </div>
