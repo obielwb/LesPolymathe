16981827cbce3a1585dcfec362e2555c5c277b1a
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-01-29 16:56:08 -0300

    feat: made a few improvements on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-
-import { Suspense, useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const Home: NextPage = () => {
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+
   return (
     <div>
+      <main className="home-app">
+        <section className="landing-page">
+
+          <div className="subscription">
+            <p>Wanna receive the knowledge to became a polymath?</p>
+            <p id="subscription-title">Subscribe in our <br /> newsletter</p>
+            <form action="">
+              <input type="text" name="User name" id="user-name" placeholder="Name" />
+              <input type="submit" value="" placeholder="youremail@example.com" />
+              <button type="submit"></button>
+            </form>
+          </div>
+        </section>
+        <section className="presentation">
+
+        </section>
+        <section className="why-subscribe">
+
+        </section>
+        <section className="final-subscribe-area">
+
+        </section>
+      </main>
     </div>
   );
 };
